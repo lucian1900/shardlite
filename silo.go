4d1d81d098af3b214ffe937aa47cfbcfb35770f0
Lucian Brănescu-Mihăilă <lucian.branescu@gmail.com>
2021-03-16 02:00:06 +0000

    Switch to shard self-timeout.

--- silo.go
+++ silo.go
@@ -47,24 +47,35 @@ func (s *LocalLockMaker) Make(id string) sync.Locker {
 }
 
 type Shard struct {
-	id       string
-	storage  Storer
-	lock     *sync.Mutex
-	lease    sync.Locker
-	lastUsed time.Time
-	dirPath  string
-	db       *sql.DB
+	id           string
+	dirPath      string
+	ttl          time.Duration
+	active       bool
+	db           *sql.DB
+	lastUsed     time.Time
+	lock         *sync.Mutex
+	deactivateCh chan string
+	stopCh       chan bool
+	storage      Storer
+	lease        sync.Locker
 }
 
-func newShard(id string, dirPath string, storage Storer, lease sync.Locker) *Shard {
+func newShard(
+	id string, dirPath string, ttl time.Duration,
+	deactivateCh chan string, storage Storer, lease sync.Locker,
+) *Shard {
 	return &Shard{
-		id:       id,
-		storage:  storage,
-		lock:     &sync.Mutex{},
-		lease:    lease,
-		lastUsed: time.Now(),
-		dirPath:  dirPath,
-		db:       nil,
+		id:           id,
+		dirPath:      dirPath,
+		ttl:          ttl,
+		active:       false,
+		db:           nil,
+		lastUsed:     time.Now(),
+		lock:         &sync.Mutex{},
+		deactivateCh: deactivateCh,
+		stopCh:       make(chan bool),
+		storage:      storage,
+		lease:        lease,
 	}
 }
 
@@ -76,98 +87,121 @@ func (s *Shard) path() string {
 }
 
 func (s *Shard) connect() *sql.DB {
-	log.Printf("Path %v", s.path())
+	//log.Printf("Path %v", s.path())
 
 	db, err := sql.Open("sqlite3", s.path())
 	try(err)
+
+	// TODO run migrations
 	return db
 }
 
-func (s *Shard) DB() *sql.DB {
+func (s *Shard) Activate() *sql.DB {
 	s.lock.Lock()
 	defer s.lock.Unlock()
-	s.lastUsed = time.Now()
 
-	if s.db == nil {
-		log.Println("Loading shard")
+	if !s.active {
+		log.Printf("Loading shard %v", s.id)
 		s.storage.Load(s.id, s.path())
 		s.db = s.connect()
+
+		go func() {
+			for {
+				select {
+				case <-time.After(s.ttl):
+					unused := time.Since(s.lastUsed)
+					if unused >= s.ttl {
+						s.Deactivate()
+					}
+				case <-s.stopCh:
+					return
+				}
+			}
+		}()
+		s.active = true
 	}
 
+	s.lastUsed = time.Now()
 	return s.db
 }
 
-func (s *Shard) save() {
-	s.storage.Save(s.id, s.path())
-}
+func (s *Shard) Deactivate() {
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-func (s *Shard) Close() {
-	log.Printf("Closing shard %v", s.id)
-	s.save()
-	s.db = nil
+	if !s.active {
+		return
+	}
+
+	log.Printf("Saving shard %v", s.id)
+	s.storage.Save(s.id, s.path())
+	err := s.db.Close()
+	try(err)
+
+	s.deactivateCh <- s.id
+	s.active = false
 }
 
 type Silo struct {
-	shards    map[string]*Shard
-	lock      *sync.Mutex
-	shardTtl  time.Duration
-	storage   Storer
-	lockMaker LockMaker
-	quitCh    chan bool
-	dirPath   string
+	started      bool
+	shards       map[string]*Shard
+	lock         *sync.Mutex
+	shardTtl     time.Duration
+	storage      Storer
+	lockMaker    LockMaker
+	stopCh       chan bool
+	deactivateCh chan string
+	dirPath      string
 }
 
 func NewSilo() *Silo {
-	silo := &Silo{
-		shards:    make(map[string]*Shard),
-		lock:      &sync.Mutex{},
-		shardTtl:  time.Duration(15 * time.Second),
-		storage:   &NullStorage{},
-		lockMaker: &LocalLockMaker{},
-		quitCh:    make(chan bool, 1),
-		dirPath:   "./dbs",
+	return &Silo{
+		started:      false,
+		shards:       make(map[string]*Shard),
+		lock:         &sync.Mutex{},
+		shardTtl:     time.Duration(5 * time.Second),
+		storage:      &NullStorage{},
+		lockMaker:    &LocalLockMaker{},
+		stopCh:       make(chan bool, 1),
+		deactivateCh: make(chan string),
+		dirPath:      "dbs",
+	}
+}
+
+func (s *Silo) Start() {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if s.started {
+		return
 	}
 
 	err := os.MkdirAll(silo.dirPath, os.ModePerm)
 	try(err)
 
-	// Expire in the background
 	go func() {
-		ticker := time.Tick(5 * time.Second)
-
 		for {
 			select {
-			case <-ticker:
-				silo.expire()
-			case <-silo.quitCh:
+			case shardId := <-silo.deactivateCh:
+				silo.removeShard(shardId)
+			case <-silo.stopCh:
 				return
 			}
 		}
 	}()
 
-	return silo
+	s.started = true
 }
 
-func (s *Silo) Shard(id string) *Shard {
+func (s *Silo) Stop() {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	log.Println("Finding shard")
-
-	shard, ok := s.shards[id]
-	if !ok {
-		shard = newShard(id, s.dirPath, s.storage, s.lockMaker.Make(id))
-		s.shards[id] = shard
+	if !s.started {
+		return
 	}
 
-	return shard
-}
-
-func (s *Silo) Shutdown() {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-
-	s.quitCh <- true
+	s.stopCh <- true
 	log.Println("Shutting down")
 
 	wg := &sync.WaitGroup{}
@@ -178,37 +212,33 @@ func (s *Silo) Shutdown() {
 			shard.lock.Lock()
 			defer shard.lock.Unlock()
 
-			shard.Close()
+			shard.Deactivate()
 			wg.Done()
 		}(shard)
 	}
 
 	wg.Wait()
+	s.started = false
 }
 
-func (s *Silo) expire() {
+func (s *Silo) Shard(id string) *Shard {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	cutoff := time.Now().Add(-s.shardTtl)
-
-	for _, shard := range s.shards {
-		if shard.lastUsed.After(cutoff) {
-			continue
-		}
-
-		// Save in parallel, without holding the silo lock
-		go func(shard *Shard) {
-			shard.lock.Lock()
-			defer shard.lock.Unlock()
-
-			shard.Close()
-
-			s.lock.Lock()
-			defer s.lock.Unlock()
-			delete(s.shards, shard.id)
-		}(shard)
+	shard, ok := s.shards[id]
+	if !ok {
+		shard = newShard(id, s.dirPath, s.shardTtl, s.deactivateCh, s.storage, s.lockMaker.Make(id))
+		s.shards[id] = shard
 	}
+
+	return shard
+}
+
+func (s *Silo) removeShard(id string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	delete(s.shards, id)
 }
 
 var silo = NewSilo()
@@ -216,7 +246,7 @@ var silo = NewSilo()
 func handler(w http.ResponseWriter, r *http.Request) {
 	userID := r.Header.Get("X-User")
 	shard := silo.Shard(userID)
-	db := shard.DB()
+	db := shard.Activate()
 
 	db.Exec("INSERT INTO counters (count) VALUES (1)")
 
@@ -229,6 +259,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	silo.Start()
+
 	http.HandleFunc("/", handler)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
